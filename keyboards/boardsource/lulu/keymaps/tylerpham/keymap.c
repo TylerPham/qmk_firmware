d7fc9fa323c058a06ed75ff12e7e062b7594dcda
tylerpham <phamtyler94@gmail.com>
2025-08-09 14:16:57 -0400

    add esc and tab as mod taps with layers

--- keyboards/boardsource/lulu/keymaps/tylerpham/keymap.c
+++ keyboards/boardsource/lulu/keymaps/tylerpham/keymap.c
@@ -26,9 +26,6 @@ enum layers {
     _ADJUST
 };
 
-#define RAISE MO(_RAISE)
-#define LOWER MO(_LOWER)
-
 // ─────────────────────────────────────────────────────────────────────────────
 // Keymaps
 // ─────────────────────────────────────────────────────────────────────────────
@@ -44,19 +41,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  [    |    |   ]   |------+------+------+------+------+------|
  * |Shift |  Z   |  X   |  C   |  V   |  B   |-------|    |-------|  N   |  M   |  ,   |  .   |  /   |Shift |
  * `-----------------------------------------/       /     \     \-----------------------------------------'
- *                  | LAlt |Lower | Bspc |  / Del  /        \Enter\  | Space|Raise | RGUI |
- *                  |      |      |      | /      /          \      \ |      |      |      |
- *                  `-----------------------------'            '-----------------------------'
+ *                  | LAlt |Lower/Esc|Bspc  | / Del  /        \Enter\  |Space |Raise/Tab| RGUI |
+ *                  |      |         |      |/       /          \     \ |      |         |      |
+ *                  `-----------------------'         '-------------------------'
  */
 [_QWERTY] = LAYOUT(
     KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
     KC_ESC,   HM_A,    HM_S,    HM_D,    HM_F,    KC_G,                       KC_H,    HM_J,    HM_K,    HM_L,    HM_SCLN, KC_QUOT,
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                               KC_LALT,  LOWER,   KC_BSPC, KC_DEL,  KC_ENT,   KC_SPC,  RAISE,   KC_RGUI
+                               KC_LALT,  LT(_LOWER, KC_ESC), KC_BSPC, KC_DEL,  KC_ENT, KC_SPC,  LT(_RAISE, KC_TAB), KC_RGUI
 ),
 
-/* LOWER 
+/* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -68,14 +65,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                  |      |      |      | /       /        \      \ |      |      |      |
  *                  |      |      |      |/       /          \      \|      |      |      |
- *                  `----------------------------'            '----------------------------'
+ *                  `----------------------------'            '--------------------'
  */
 [_LOWER] = LAYOUT(
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_CIRC, KC_LCBR, KC_RCBR, KC_DLR,  XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_HASH, KC_LPRN, KC_RPRN, KC_SCLN, KC_DQUO, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_AT,   KC_COLN, KC_COMM, KC_DOT,  KC_QUOT, XXXXXXX,
-                               _______, _______, _______, _______, _______,  _______, _______, _______
+                               _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* RAISE
@@ -90,17 +87,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                  |      |      |      | /       /        \      \ |      |      |      |
  *                  |      |      |      |/       /          \      \|      |      |      |
- *                  `----------------------------'            '----------------------------'
+ *                  `----------------------------'            '--------------------'
  */
 [_RAISE] = LAYOUT(
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, KC_GRV,  KC_LABK, KC_RABK, KC_MINS, KC_PIPE,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, KC_EXLM, KC_ASTR, KC_SLSH, KC_EQL,  KC_AMPR,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, KC_TILD, KC_PLUS, KC_LBRC, KC_RBRC, KC_PERC, RM_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                               _______, _______, _______, _______, _______,  _______, _______, _______
+    XXXXXXX, KC_TILD, KC_PLUS, KC_LBRC, KC_RBRC, KC_PERC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                               _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
-/* ADJUST 
+/* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -112,17 +109,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                  |      |      |      | /       /        \      \ |      |      |      |
  *                  |      |      |      |/       /          \      \|      |      |      |
- *                  `----------------------------'            '----------------------------'
+ *                  `----------------------------'            '--------------------'
  */
 [_ADJUST] = LAYOUT(
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                               _______, _______, _______, _______, _______,  _______, _______, _______
+                               _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
-}; 
+};
 
 // ─────────────────────────────────────────────────────────────────────────────
 // Tri-layer handling
