c931a06d378667fcf52421d009afbc2991d64a2a
TylerPham <phamtyler94@gmail.com>
2025-12-19 14:32:48 -0500

    Add gaming layer to keymap
    - Introduced a new _GAMING layer that mirrors QWERTY layout but modifies certain keys for gaming use.
    - Updated the _ADJUST layer to include a direct switch to the new _GAMING layer.

--- keyboards/boardsource/lulu/keymaps/tylerpham/keymap.c
+++ keyboards/boardsource/lulu/keymaps/tylerpham/keymap.c
@@ -32,6 +32,7 @@
 // ─────────────────────────────────────────────────────────────────────────────
 enum layers {
     _QWERTY,
+    _GAMING,
     _RAISE,
     _LOWER,
     _ADJUST
@@ -83,6 +84,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   HM_A,    HM_S,    HM_D,    HM_F,    KC_G,                       KC_H,    HM_J,    HM_K,    HM_L,    HM_SCLN, KC_QUOT,
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                                KC_LALT,  LT(_LOWER, KC_ESC), KC_BSPC, KC_DEL,  KC_ENT, KC_SPC,  LT(_RAISE, KC_TAB), KC_BSLS
+),
+
+/* GAMING
+ * Same as QWERTY but without home row mods; Del -> Ctrl, Bslash -> GUI
+ */
+[_GAMING] = LAYOUT(
+    KC_EQUAL,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_GRV,
+    KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                               KC_LALT,  LT(_LOWER, KC_ESC), KC_BSPC, KC_LCTL,  KC_ENT, KC_SPC,  LT(_RAISE, KC_TAB), KC_LGUI
 ),
 
 /* LOWER
@@ -145,7 +157,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT(
     QK_MAKE, QK_BOOTLOADER, KC_GRV, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    CG_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
+    CG_TOGG, DF(_QWERTY), DF(_GAMING), XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                _______, _______, _______, _______, _______, _______, _______, _______
